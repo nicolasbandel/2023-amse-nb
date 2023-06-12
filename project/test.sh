11f952e2d4e1ae0c6ffebe8c802af7cf9a28ef2d
N B <nicolas.bandel@fau.de>
2023-06-12 13:32:00 +0200

    ..

--- project/test.sh
+++ project/test.sh
@@ -1,5 +1,5 @@
 echo "loading requiredments"
-pip ./main/project/requirements.txt 
+pip install -r ./main/project/requirements.txt 
 echo "running test.py"
 python ./main/project/tests.py
 find ./ -type f 
